b76ecdddec2c0bfecbcfffe203e624115629ac09
Valentin Thibault <valentin.thlt@gmail.com>
2024-08-02 14:10:50 +0200

    feat(endpoint): connecting all endpoint to API

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const connectDB = require('./config/db.js')
 const UserRouter = require('./routes/user.route.js')
+const QuestionRouter = require('./routes/question.route.js')
+const ScoreRouter = require('./routes/score.route.js')
 
 
 const app = express()
@@ -10,7 +12,11 @@ connectDB()
 app.use(express.json({ extended: false }))
 
 const userRouter = new UserRouter()
+const questionRouter = new QuestionRouter()
+const scoreRouter = new ScoreRouter()
 app.use(userRouter.basePath, userRouter.getRouter())
+app.use(questionRouter.basePath, questionRouter.getRouter())
+app.use(scoreRouter.basePath, scoreRouter.getRouter())
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
