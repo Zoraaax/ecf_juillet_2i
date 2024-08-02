dbcd7d463f0312ffcd2d0b4c03469f911164993e
Valentin Thibault <valentin.thlt@gmail.com>
2024-08-02 10:30:32 +0200

    feat(model): create user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        maxlength: 80
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 256
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    progress: {
+        currentLevel: {
+            type: Number,
+            default: 1
+        },
+        currentScore: {
+            type: Number,
+            default: 0
+        },
+        maxScore: {
+            type: Number,
+            default: 0
+        }
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
